e9d072b15f0f5d931f1e8381917ed48d7a068ee3
evalece <60049803+evalece@users.noreply.github.com>
2025-09-15 22:20:36 -0400

    need to redo with useMemo, not started yet

--- src/app/components/widget/WidgetAffordabilityPlot.tsx
+++ src/app/components/widget/WidgetAffordabilityPlot.tsx
@@ -10,14 +10,54 @@ Consider provider pattern if place[] is shared in multiple component
 import { ReactNode } from "react";
 
 import React, { useEffect, useState } from "react";
-import { AffordabilityPlot } from "../cards/AffordabilityPlot";
-import {Place} from "../../lib/NearByGooglePlace";
+import AffordabilityPlot from "../cards/AffordabilityPlot"
 
-export function WidgetAffordabilityPlot({place}:{place:Place[]}){
+import { Place,NearByGooglePlace } from "../../lib/NearByGooglePlace";
+
+type Props= {storeType: string; maxResult:number}; // same for nearBy related API calls
+
+/*
+to dos: 
+
+useMemo to avoid redundant computation in plot, 
+*/
+
+export function WidgetAffordabilityPlot({ storeType, maxResult }: Props){
+
+const [error, setError]=useState <string|null> (null);
+const [loading, setLoading]= useState<boolean>(false);
+const [reseult, setResult]=useState<>
+    
     useEffect((()=>{
- 
+        let cancelled= false;
+
+        (async()=>{
+
+        
+
+            try{
+            setLoading(true);
+             const place=await NearByGooglePlace({ storeType, maxResult }); 
+            }catch (e:any){
+                 if (!cancelled) setError(e?.message ?? "Failed to load places");
+            }  finally {
+            if (!cancelled) setLoading(false);
+            }
+
+            } // end of async
+           
+        )
+
+
     }
-    ),[])
+    ),[])// end of useEffect
+
+
+    // Early return rendering while loading;  
+    if (loading) return <div> Budget Map is loading ... </div>
+    if (error) return <div role="alert">Error: {error}</div>;   
+
+    // rendering here:
     return 
 
 }
